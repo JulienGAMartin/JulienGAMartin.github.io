89d4be1c6c5b689727527a750b90d60ff547b5a2
Julien Martin <julien.ga.martin@gmail.com>
2018-02-07 16:33:55 +0000

    first website on github

---
+++ content/themes/footer.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * @package WordPress
+ * @subpackage Coraline
+ * @since Coraline 1.0
+ */
+?>
+	</div><!-- #content-box -->
+
+	<div id="footer" role="contentinfo">
+		<?php get_sidebar( 'footer' ); ?>
+
+		<div id="colophon">
+<a href="http://pages.usherbrooke.ca/jmartin">
+<img src="../wp-content/themes/coraline-child/foot/foot1.jpg" alt="" width="150" height="100" />
+<img src="../wp-content/themes/coraline-child/foot/foot2.jpg" alt="" width="150" height="100" />
+<img src="../wp-content/themes/coraline-child/foot/foot3.jpg" alt="" width="150" height="100" />
+<img src="../wp-content/themes/coraline-child/foot/foot4.jpg" alt="" width="150" height="100" />
+<img src="../wp-content/themes/coraline-child/foot/foot5.jpg" alt="" width="150" height="100" />
+<img src="../wp-content/themes/coraline-child/foot/foot6.jpg" alt="" width="150" height="100" />
+
+</a>
+</br></br>
+<span class="generator-link"><a href="<?php echo esc_url( __( 'http://wordpress.org/', 'coraline' ) ); ?>" title="<?php esc_attr_e( 'A Semantic Personal Publishing Platform', 'coraline' ); ?>" rel="generator"><?php printf( __( 'Proudly powered by %s.', 'coraline' ), 'WordPress' ); ?></a></span>
+		</div><!-- #colophon -->
+	</div><!-- #footer -->
+
+</div><!-- #container -->
+
+<?php wp_footer(); ?>
+</body>
+</html>
